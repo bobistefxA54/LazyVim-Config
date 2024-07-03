4cb575174419024728962a932c62a0c2f2dd4590
bobistefxA54 <borislav.stefanov.a54@learn.telerikacademy.com>
2024-07-03 11:52:06 +0300

    update default colorscheme setting and add custom remaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
